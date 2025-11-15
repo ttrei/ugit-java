c2d9331f5189aaf56ef6d8eecc2fb030454f57b4
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-11-15 23:47:04 +0200

    done: https://www.leshenko.net/p/ugit/#cli-argparse

---
+++ app/src/main/java/lv/taukulis/MicroGit.java
@@ -0,0 +1,24 @@
+package lv.taukulis;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name="ugit", mixinStandardHelpOptions = true,
+        subcommands = {InitCommand.class, CommandLine.HelpCommand.class})
+public class MicroGit {
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new MicroGit()).execute(args);
+        System.exit(exitCode);
+    }
+}
+
+@Command(name="init", mixinStandardHelpOptions = true)
+class InitCommand implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Hello from ugit init!");
+    }
+
+}
---
+++ app/src/test/java/lv/taukulis/AppTest.java
@@ -0,0 +1,12 @@
+package lv.taukulis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test void dummyTest() {
+        MicroGit classUnderTest = new MicroGit();
+        assertTrue(true);
+    }
+}
