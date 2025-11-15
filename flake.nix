2125234a7605cc57fc9c65c0e66f63b38271ff5d
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-11-15 11:00:14 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Java Playground";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ pkgs.gradle ];
+    };
+  };
+}
