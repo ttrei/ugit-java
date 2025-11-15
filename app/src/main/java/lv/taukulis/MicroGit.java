f5afbc92d1eb78b52c3ce9143f79b9dcfcc0d091
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-11-15 23:53:35 +0200

    copy-paste from example

--- app/src/main/java/lv/taukulis/MicroGit.java
+++ app/src/main/java/lv/taukulis/MicroGit.java
@@ -2,10 +2,14 @@ package lv.taukulis;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Spec;
 
 @Command(name="ugit", mixinStandardHelpOptions = true,
         subcommands = {InitCommand.class, CommandLine.HelpCommand.class})
 public class MicroGit {
+    @SuppressWarnings("unused")
+    @Spec CommandSpec spec;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new MicroGit()).execute(args);
