d7dcd158df9013fd8112539126b2261e7a0be76e
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-11-16 15:37:45 +0200

    done: https://www.leshenko.net/p/ugit/#hash-object

--- app/src/main/java/lv/taukulis/Data.java
+++ app/src/main/java/lv/taukulis/Data.java
@@ -4,19 +4,35 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Data {
 
     private static final String GIT_DIR = ".ugit";
 
-    public static void init() {
-
+    public static void init() throws IOException {
         Path ugitPath = Paths.get(GIT_DIR);
+        Path objectsPath = Paths.get(GIT_DIR, "objects");
+        Files.createDirectory(ugitPath);
+        Files.createDirectory(objectsPath);
+        System.out.println("Initialized empty ugit repository at " + ugitPath.toAbsolutePath());
+    }
+
+    public static void hashObject(Path file) throws IOException {
         try {
-            Files.createDirectory(ugitPath);
-            System.out.println("Initialized ugit repository at " + ugitPath.toAbsolutePath());
-        } catch (IOException e) {
-            System.err.println("Failed to create directory: " + e.getMessage());
+            hashObject(file, "SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("SHA-1 algorithm not available", e);
         }
     }
+
+    public static void hashObject(Path file, String algorithm) throws NoSuchAlgorithmException, IOException {
+        byte[] data = Files.readAllBytes(file);
+        MessageDigest md = MessageDigest.getInstance(algorithm);
+        String objectId = Utils.bytesToHex(md.digest(data));
+        Path objectPath = Paths.get(GIT_DIR, "objects", objectId);
+        Files.write(objectPath, data);
+    }
+
 }
--- app/src/main/java/lv/taukulis/MicroGit.java
+++ app/src/main/java/lv/taukulis/MicroGit.java
@@ -3,13 +3,21 @@ package lv.taukulis;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Spec;
 
-@Command(name="ugit", mixinStandardHelpOptions = true,
-        subcommands = {InitCommand.class, CommandLine.HelpCommand.class})
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@Command(name = "ugit", mixinStandardHelpOptions = true, subcommands = {InitCommand.class, HashObjectCommand.class,
+        CommandLine.HelpCommand.class})
 public class MicroGit {
     @SuppressWarnings("unused")
-    @Spec CommandSpec spec;
+    @Spec
+    CommandSpec spec;
 
     public static void main(String[] args) {
         int exitCode = new CommandLine(new MicroGit()).execute(args);
@@ -17,12 +25,29 @@ public class MicroGit {
     }
 }
 
-@Command(name="init", mixinStandardHelpOptions = true)
-class InitCommand implements Runnable {
+@Command(name = "init", mixinStandardHelpOptions = true)
+class InitCommand implements Callable<Integer> {
 
     @Override
-    public void run() {
+    public Integer call() throws IOException {
         Data.init();
+        return 0;
+    }
+
+}
+
+@Command(name = "hash-object", mixinStandardHelpOptions = true)
+class HashObjectCommand implements Callable<Integer> {
+
+    @Parameters(arity = "1..*", description = "Files to hash")
+    List<Path> files;
+
+    @Override
+    public Integer call() throws IOException {
+        for (Path file : files) {
+            Data.hashObject(file);
+        }
+        return 0;
     }
 
 }
