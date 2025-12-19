a8c639c7977c7a8dce328e037675ab361062620d
Reinis Taukulis <reinis.taukulis@gmail.com>
2025-12-20 01:29:11 +0200

    remove gradle2nix - copied flake.nix to home.taukulis.lv java playground
    i will not deploy ugit anywhere, no reason to build it via nix

--- flake.nix
+++ flake.nix
@@ -2,35 +2,12 @@
   description = "ugit";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    gradle2nix.url = "github:tadfisher/gradle2nix/v2";
   };
-  outputs = { self, nixpkgs, gradle2nix }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages.${system}.default =
-        gradle2nix.builders.${system}.buildGradlePackage {
-          pname = "ugit";
-          version = "0.1";
-
-          src = ./.;
-          # generated with 'nix run github:tadfisher/gradle2nix/v2'
-          lockFile = ./gradle.lock;
-
-          gradleBuildFlags = [ "shadowJar" ];
-
-          installPhase = ''
-            mkdir -p $out/bin $out/lib
-            cp build/libs/*-all.jar $out/lib/app.jar
-
-            cat > $out/bin/ugit <<EOF
-            #!${pkgs.bash}/bin/bash
-            exec ${pkgs.jre}/bin/java -jar $out/lib/app.jar "\$@"
-            EOF
-            chmod +x $out/bin/ugit
-          '';
-        };
 
       devShells.default = pkgs.mkShell {
         buildInputs = with pkgs; [
